0694c0e08dcd72b47dab64ae75426841bb3bb151
mcarbenay <michael@carbenay.info>
2020-08-10 19:43:13 +0200

    Premier exemple : vérifier le stock à la commande

---
+++ ecommerce/CheckStock/PanierCheckStock/QuantiteLimiteCheckStock.cs
@@ -0,0 +1,57 @@
+﻿using CPointSoftware.Equihira.Extensibility.ECommerce;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanierCheckStock
+{
+    [Export(typeof(IPanierStockCheck))]
+    public class QuantiteLimiteCheckStock : IPanierStockCheck
+    {
+        public bool CheckStock(Guid art_guid, ref decimal quantite, Guid? magasin, string typeCommandable)
+        {
+            // Really simple "fake check" : if guid is odd
+            // then only 1 is available, if even, 2 is available
+            // if >= 'a' then 3
+            var t = art_guid.ToString("N");
+            t = t.Substring(t.Length - 1);
+            if (t[0] >= '0' && t[0] <= '9')
+            {
+                int i = int.Parse(t);
+                if (i % 2 == 1)
+                {
+                    if (quantite > 1)
+                    {
+                        quantite = 1;
+                        return false;
+                    }
+                    else
+                        return true;
+                }
+                else
+                {
+                    if (quantite > 2)
+                    {
+                        quantite = 2;
+                        return false;
+                    }
+                    else
+                        return true;
+                }
+            }
+            else
+            {
+                if (quantite <= 3)
+                    return true;
+                else
+                {
+                    quantite = 3;
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ ecommerce/CheckStock/PanierCheckStock/readme.md
@@ -0,0 +1,36 @@
+# Vérification du stock à la commande
+
+Interface à implémenter : `CPointSoftware.Equihira.Extensibility.ECommerce.IPanierStockCheck`
+
+```csharp
+namespace CPointSoftware.Equihira.Extensibility.ECommerce
+{
+    public interface IPanierStockCheck
+    {
+        bool CheckStock(Guid art_guid, ref decimal quantite, Guid? magasin, string typeCommandable);
+    }
+}
+```
+
+Il n'y a qu'une méthode à implémenter : elle sera appelée à chaque changement de quantité dans le panier pour un produit. 
+
+Vous devrez :
+
+- vérifier la disponibilité de votre produit et renvoyer true si la quantité est disponible et peut être commandée
+- renvoyer false ET mettre à jour la quantité avec le maximum commandable si tout n'est pas disponible.
+- Si le `Guid` magasin est non `null`, le test de quantité doit être effectué dans le cadre de ce magasin (pour un achat emporté depuis le stock du magasin), si il est `null`, il s'agit d'un achat de type Web.
+
+> TODO : vérifier si elle est appelée dans le cadre d'un produit immatériel
+
+Comme toute extension MEF, vous devrez ajouter un attribut `[Export]` sur la classe pour la rendre détectable en tant qu'extension.
+
+## Déploiement
+
+Une fois réalisée vous devrez déposer la dll avec cette classe dans votre dossier d'extensibilité, sous Ecommerce\Assemblies. Pour plus de renseignement sur les dossiers d'extensibilité, [consultez l'aide en ligne](https://www.altazion.dev/global/index.html#paths-importants).
+
+[!Dossier](checkstock-0.png)
+
+## Exemple
+
+[!Ajout](checkstock-1.png)
+[!Erreur](checkstock-2.png)
