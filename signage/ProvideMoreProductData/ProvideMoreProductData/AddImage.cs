dc0bfa3daf04af754b97a0fc76861a16ebe6d0b2
Michael CARBENAY <michael@carbenay.info>
2021-05-18 08:40:56 +0200

    commentaire dans le code pour l'exemple phygital

--- signage/ProvideMoreProductData/ProvideMoreProductData/AddImage.cs
+++ signage/ProvideMoreProductData/ProvideMoreProductData/AddImage.cs
@@ -9,9 +9,20 @@ using System.Threading.Tasks;
 
 namespace ProvideMoreProductData
 {
-    [Export(typeof(IProvideMoreProductData))]
+    /// <summary>
+    /// Cette classe ajoute une images dans la partie
+    /// "images complémentaires" de la fiche produit
+    /// </summary>
+    [Export(typeof(IProvideMoreProductData))] // ne pas oublier l'export !
     public class AddImage : IProvideMoreProductData
     {
+        /// <summary>
+        /// Méthode à implémenter pour modifier le contenu
+        /// de la fiche produit
+        /// </summary>
+        /// <param name="rjs_id">La raison juridique concernée</param>
+        /// <param name="sit_pk">Le site affichant les données</param>
+        /// <param name="details">La fiche produit à compléter/modifier</param>
         public void AddInfoToDetails(int rjs_id, int sit_pk, ArticlePhygitalDetail details)
         {
             List<ArticleImage> imgs = new List<ArticleImage>();
@@ -25,8 +36,18 @@ namespace ProvideMoreProductData
             details.MoreImages = imgs.ToArray();
         }
 
+        /// <summary>
+        /// Méthode à implémenter pour modifier le contenu
+        /// la descente produits
+        /// </summary>
+        /// <param name="rjs_id">La raison juridique concernée</param>
+        /// <param name="sit_pk">Le site affichant les données</param>
+        /// <param name="items">Les produits de la descente</param>
         public void AddInfoToList(int rjs_id, int sit_pk, ArticlePhygitalBase[] items)
         {
+            // on ne modifie rien sur les descentes : 
+            // on peut au choix, soit laisser le corps
+            // de la méthode vide ou lever une NotImplementedException
             throw new NotImplementedException();
         }
     }
--- signage/ProvideMoreProductData/ProvideMoreProductData/ChangeNameOfProducts.cs
+++ signage/ProvideMoreProductData/ProvideMoreProductData/ChangeNameOfProducts.cs
@@ -9,20 +9,42 @@ using System.Threading.Tasks;
 
 namespace ProvideMoreProductData
 {
-    [Export(typeof(IProvideMoreProductData))]
+    /// <summary>
+    /// Cette classe modifie les contenus de la
+    /// fiche produit et des descentes en inversant
+    /// tous les caractères du libellé des articles
+    /// </summary>
+    [Export(typeof(IProvideMoreProductData))] // ne pas oublier l'export !
     public class ChangeNameOfProducts : IProvideMoreProductData
     {
+
+        /// <summary>
+        /// Méthode à implémenter pour modifier le contenu
+        /// de la fiche produit
+        /// </summary>
+        /// <param name="rjs_id">La raison juridique concernée</param>
+        /// <param name="sit_pk">Le site affichant les données</param>
+        /// <param name="details">La fiche produit à compléter/modifier</param>
         public void AddInfoToDetails(int rjs_id, int sit_pk, ArticlePhygitalDetail details)
         {
+            // comme exemple : on inverse le libellé de l'article
             if (details.Libelle != null)
                 details.Libelle = new string(details.Libelle.Reverse().ToArray());
         }
 
+        /// <summary>
+        /// Méthode à implémenter pour modifier le contenu
+        /// la descente produits
+        /// </summary>
+        /// <param name="rjs_id">La raison juridique concernée</param>
+        /// <param name="sit_pk">Le site affichant les données</param>
+        /// <param name="items">Les produits de la descente</param>
         public void AddInfoToList(int rjs_id, int sit_pk, ArticlePhygitalBase[] items)
         {
             if (items == null)
                 return;
 
+            // comme exemple : on inverse le libellé de l'article
             foreach (var details in items)
             {
                 if (details.Libelle != null)
